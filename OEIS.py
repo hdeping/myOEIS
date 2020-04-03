dd52331b0d59f4978a1804275f75c0c39c7fa42a
黄德平(Deping Huang, Macbook Air) <xiaohengdao@gmail.com>
2020-04-03 21:56:52 +0800

    add class OEIS

---
+++ OEIS.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+ 
+"""
+
+============================
+
+    @author       : Deping Huang
+    @mail address : xiaohengdao@gmail.com
+    @date         : 2020-04-03 21:55:38
+    @project      : OEIS sequence
+    @version      : 1.0
+    @source file  : OEIS.py
+
+============================
+"""
+
+class OEIS():
+    """docstring for OEIS"""
+    def __init__(self):
+        super(OEIS, self).__init__()
+    def A000108Catalan(self,n=20):
+        """
+        docstring for A000108Catalan
+        """
+        
+        return
+
+
+
+
+
+        
